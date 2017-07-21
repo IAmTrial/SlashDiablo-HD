55f220f261ee812caadda6024004943986ac06e5
IAmTrial <IAmRealTrial@gmail.com>
2017-07-21 16:00:10 -0700

    Port over all relevant pointers. - 1.13d crashes, and cause is unknown. - Change the name of DrawImage to DrawCellContext.

--- D2Ptrs.h
+++ D2Ptrs.h
@@ -62,7 +62,7 @@ struct PointerOffset {
 *   D2CMP.DLL POINTERS                                                          *
 *                                                                               *
 *********************************************************************************/
-D2FUNC(D2CMP, FreeCellFile, BOOL, __stdcall, (CellFile* pCellFile), 0x11520, -1); // Called at D2Client.dll+26E1C,8022E
+D2FUNC(D2CMP, FreeCellFile, BOOL, __stdcall, (CellFile* pCellFile), 0x11520, 0x135B0); // Called at D2Client.dll+26E1C,8022E
 
 /********************************************************************************
 *                                                                               *
@@ -76,21 +76,21 @@ D2FUNC(D2CMP, FreeCellFile, BOOL, __stdcall, (CellFile* pCellFile), 0x11520, -1)
 *   D2CLIENT.DLL POINTERS                                                       *
 *                                                                               *
 *********************************************************************************/
-D2VAR(D2CLIENT, ScreenSizeX, DWORD, 0xDBC48, -1);
-D2VAR(D2CLIENT, ScreenSizeY, DWORD, 0xDBC4C, -1);
-D2VAR(D2CLIENT, PanelOffsetX, int, 0x11B9A0, -1);
-D2VAR(D2CLIENT, PanelOffsetY, int, 0x11B9A4, -1);
-D2VAR(D2CLIENT, InventoryArrangeMode, int, 0x11B99C, -1);
-D2VAR(D2CLIENT, PanelBorderImage, CellFile*, 0x11A77C, -1);
-D2VAR(D2CLIENT, PanelOpenMode, int, 0x11C414, -1);
-D2VAR(D2CLIENT, MaxRegistryResolutionMode, int, 0xEABD8, -1);
-D2VAR(D2CLIENT, CurrentRegistryResolutionMode, int, 0xEABDC, -1);
-D2VAR(D2CLIENT, SelectedMenuOption, int, 0x11C058, -1);
+D2VAR(D2CLIENT, ScreenSizeX, DWORD, 0xDBC48, 0xF7034);
+D2VAR(D2CLIENT, ScreenSizeY, DWORD, 0xDBC4C, 0xF7038);
+D2VAR(D2CLIENT, PanelOffsetX, int, 0x11B9A0, 0x11D354);
+D2VAR(D2CLIENT, PanelOffsetY, int, 0x11B9A4, 0x11D358);
+D2VAR(D2CLIENT, InventoryArrangeMode, int, 0x11B99C, 0x11D2B4);
+D2VAR(D2CLIENT, PanelBorderImage, CellFile*, 0x11A77C, 0x103B14);
+D2VAR(D2CLIENT, PanelOpenMode, int, 0x11C414, 0x11D070);
+D2VAR(D2CLIENT, MaxRegistryResolutionMode, int, 0xEABD8, -1); // Unused by D2HD
+D2VAR(D2CLIENT, CurrentRegistryResolutionMode, int, 0xEABDC, 0xE4F44);
+D2VAR(D2CLIENT, SelectedMenuOption, int, 0x11C058, -1); // Unused by D2HD
 
 // Do not call this function directly unless you insert arg0 into EDI
-D2FUNC(D2CLIENT, LoadUIImage, CellFile*, __fastcall, (const char* szImage), 0xBF6C0, -1);
+D2FUNC(D2CLIENT, LoadUIImage, CellFile*, __fastcall, (const char* szImage), 0xBF6C0, 0x191F0);
 
-// Only use as a reference! Called inside of D2client.dll+C39E0
+// Only use as a reference! Called inside of D2client.dll+C39E0! All unused by D2HD!
 D2FUNC(D2CLIENT, DrawMercenaryInventoryPanel, void*, __fastcall, (), 0x9AE80, -1);
 D2FUNC(D2CLIENT, DrawInfusScrollPanel, void*, __fastcall, (), 0x7DE00, -1);
 D2FUNC(D2CLIENT, DrawInventoryPanel, void*, __fastcall, (), 0x99440, -1);
@@ -104,7 +104,7 @@ D2FUNC(D2CLIENT, DrawStatPanel, void*, __fastcall, (), 0xBCEA0, -1);
 *   D2COMMON.DLL POINTERS                                                       *
 *                                                                               *
 *********************************************************************************/
-D2VAR(D2COMMON, InventoryTxt, InventoryTxt*, 0x9FA5C, -1);
+D2VAR(D2COMMON, InventoryTxt, InventoryTxt*, 0x9FA5C, 0xA4CAC);
 
 /********************************************************************************
 *                                                                               *
@@ -118,60 +118,58 @@ D2VAR(D2DDRAW, GameWindowSizeY, DWORD*, 0x101D8, -1);
 *   D2GDI.DLL POINTERS                                                          *
 *                                                                               *
 *********************************************************************************/
-D2VAR(D2GDI, ForegroundRenderWidth, DWORD, 0xCA9C, -1);
+D2VAR(D2GDI, ForegroundRenderWidth, DWORD, 0xCA9C, 0xCA98);
 
 /********************************************************************************
 *                                                                               *
 *   D2GFX.DLL POINTERS                                                          *
 *                                                                               *
 *********************************************************************************/
-D2VAR(D2GFX, GfxMode, int, 0x11258, -1);
-D2VAR(D2GFX, ResolutionMode, int, 0x11260, -1);
+D2VAR(D2GFX, GfxMode, int, 0x11258, -1); // Unused by D2HD
+D2VAR(D2GFX, ResolutionMode, int, 0x11260, 0x14A40);
 
-D2FUNC(D2GFX, GetResolutionMode, int, __stdcall, (), 0xB320, -1);
-D2FUNC(D2GFX, DrawImage, void, __stdcall, (CellContext* pCellContext, int nXpos, int nYpos, DWORD color, int nTransTbl, unsigned char* pPalette), 0xB080, -1);
+D2FUNC(D2GFX, GetResolutionMode, int, __stdcall, (), 0xB320, 0xA940);
+D2FUNC(D2GFX, DrawCellContext, void, __stdcall, (CellContext* pCellContext, int nXpos, int nYpos, DWORD color, int nTransTbl, unsigned char* pPalette), 0xB080, 0x7E60);
 
 /********************************************************************************
 *                                                                               *
 *   D2GLIDE.DLL POINTERS                                                        *
 *                                                                               *
 *********************************************************************************/
-D2VAR(D2GLIDE, ScreenSizeX, DWORD, 0x15A68, -1);
-D2VAR(D2GLIDE, ScreenSizeY, DWORD, 0x15B04, -1);
+D2VAR(D2GLIDE, ScreenSizeX, DWORD, 0x15A68, 0x15A78);
+D2VAR(D2GLIDE, ScreenSizeY, DWORD, 0x15B04, 0x15B14);
 
 /********************************************************************************
 *                                                                               *
 *   D2WIN.DLL POINTERS                                                          *
 *                                                                               *
 *********************************************************************************/
-D2FUNC(D2WIN, LoadCellFile, CellFile*, __fastcall, (const char* szFile, int Type), 0xA7A0, -1);
-D2PTR(D2WIN, LoadMpq_I, 0x7E60, -1);
+D2FUNC(D2WIN, LoadCellFile, CellFile*, __fastcall, (const char* szFile, int Type), 0xA7A0, 0xCFC0);
+D2PTR(D2WIN, LoadMpq_I, 0x7E60, 0x7E50);
 
 /********************************************************************************
 *                                                                               *
 *   GLIDE3X.DLL POINTERS                                                        *
 *                                                                               *
 *********************************************************************************/
-D2VAR(GLIDE3X, GameWindowSizeX, DWORD*, 0x1C9A0, -1);
-D2VAR(GLIDE3X, GameWindowSizeY, DWORD*, 0x1C82C, -1);
+D2VAR(GLIDE3X, GameWindowSizeX, DWORD*, 0x1C9A0, 0x1C9A0);
+D2VAR(GLIDE3X, GameWindowSizeY, DWORD*, 0x1C82C, 0x1C9A0);
 
 /********************************************************************************
 *                                                                               *
 *   FOG.DLL POINTERS                                                            *
 *                                                                               *
 *********************************************************************************/
-D2VAR(FOG, InGame, BOOL, 0x4C804, -1);
+D2VAR(FOG, InGame, BOOL, 0x4C804, 0xF79E0);
 
-D2FUNC(FOG, FreeClientMemory, void, __fastcall, (void* pMemoryToFree, const char* szFile, int nLine, void* pNull), 0x1CCF0, -1);
+D2FUNC(FOG, FreeClientMemory, void, __fastcall, (void* pMemoryToFree, const char* szFile, int nLine, void* pNull), 0x1CCF0, 0x1B350);
 
 /********************************************************************************
 *                                                                               *
 *   STORM.DLL POINTERS                                                          *
 *                                                                               *
 *********************************************************************************/
-D2VAR(STORM, IsCinematic, BOOL, 0x54D74, -1);
-
-D2FUNC(STORM, CloseArchive, BOOL, __stdcall, (HANDLE hArchive), 0x26CB0, -1) // Storm.Ordinal252
+D2FUNC(STORM, CloseArchive, BOOL, __stdcall, (HANDLE hArchive), 0x26CB0, 0x2BA50) // Storm.Ordinal252
 
 // end of file -----------------------------------------------------------------
 #endif
