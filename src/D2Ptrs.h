d13c71a7d00d9b25ecaad4b275e5d746db163415
IAmTrial <IAmRealTrial@gmail.com>
2018-03-13 10:40:18 -0700

    Change the license on every applicable file to GPLv3+.

--- src/D2Ptrs.h
+++ src/D2Ptrs.h
@@ -1,8 +1,27 @@
+/***************************************************************************
+ *                                                                         *
+ *   SlashDiablo HD                                                        *
+ *   Copyright (C) 2017  Mir Drualga                                       *
+ *                                                                         *
+ *   This program is free software: you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation, either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
 /*****************************************************************************
  *                                                                           *
  *   D2Ptrs.h                                                                *
  *   Copyright (C) Olivier Verville                                          *
- *   SlashDiablo-Tools Modifications: Copyright (C) 2017 Mir Drualga         *
  *                                                                           *
  *   Licensed under the Apache License, Version 2.0 (the "License");         *
  *   you may not use this file except in compliance with the License.        *
