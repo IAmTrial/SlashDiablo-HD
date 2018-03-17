0be1df3281cffdb26e3f6fcdcaa8a272f4b8132d
IAmTrial <IAmRealTrial@gmail.com>
2018-03-16 20:02:32 -0700

    Convert necessary license text from GPL v3+ to AGPL v3+.

--- src/D2HD/D2HDCellContext.cpp
+++ src/D2HD/D2HDCellContext.cpp
@@ -1,28 +1,28 @@
-/***************************************************************************
- *                                                                         *
- *   SlashDiablo HD                                                        *
- *   Copyright (C) 2017  Mir Drualga                                       *
- *                                                                         *
- *   This program is free software: you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation, either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- *                                                                         *
- *-------------------------------------------------------------------------*
- *                                                                         *
- *   Defines a class that provides higher level abstraction for            *
- *   CellContext, for the purpose of allowing it to work on different      *
- *   versions of the game.                                                 *
- *                                                                         *
- ***************************************************************************/
+/*****************************************************************************
+ *                                                                           *
+ *  SlashDiablo HD                                                           *
+ *  Copyright (C) 2017  Mir Drualga                                          *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU Affero General Public License as published *
+ *  by the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Affero General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU Affero General Public License *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ *   Defines a class that provides higher level abstraction for              *
+ *   CellContext, for the purpose of allowing it to work on different        *
+ *   versions of the game.                                                   *
+ *                                                                           *
+ *****************************************************************************/
 
 /*==========================================================
 * D2Ex2
--- src/D2HD/D2HDInventory.h
+++ src/D2HD/D2HDInventory.h
@@ -1,27 +1,27 @@
-/***************************************************************************
- *                                                                         *
- *   SlashDiablo HD                                                        *
- *   Copyright (C) 2017  Mir Drualga                                       *
- *                                                                         *
- *   This program is free software: you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation, either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
- *                                                                         *
- *-------------------------------------------------------------------------*
- *                                                                         *
- *   The file defines a set of functions that comes from D2Ex2 and are     *
- *   used for correcting the position of items in the inventory.           *
- *                                                                         *
- ***************************************************************************/
+/*****************************************************************************
+ *                                                                           *
+ *  SlashDiablo HD                                                           *
+ *  Copyright (C) 2017  Mir Drualga                                          *
+ *                                                                           *
+ *  This program is free software: you can redistribute it and/or modify     *
+ *  it under the terms of the GNU Affero General Public License as published *
+ *  by the Free Software Foundation, either version 3 of the License, or     *
+ *  (at your option) any later version.                                      *
+ *                                                                           *
+ *  This program is distributed in the hope that it will be useful,          *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ *  GNU Affero General Public License for more details.                      *
+ *                                                                           *
+ *  You should have received a copy of the GNU Affero General Public License *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                           *
+ *---------------------------------------------------------------------------*
+ *                                                                           *
+ *   The file defines a set of functions that comes from D2Ex2 and are       *
+ *   used for correcting the position of items in the inventory.             *
+ *                                                                           *
+ *****************************************************************************/
 
 /*==========================================================
 * D2Ex2
